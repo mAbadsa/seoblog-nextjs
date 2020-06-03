f77d2dc0d39d8b695f0a64522750e8753fe8fdeb
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-03 15:47:14 +0300

    Create New User Is Filished

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,32 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const shortId = require("shortid");
 
-const getSignup = async (req, res) => {
-  res.json({
-    success: true,
-    data: [],
-    message: "Signup",
+const signup = async (req, res) => {
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({ error: "Email is taken" });
+    }
+
+    const { name, email, password } = req.body;
+    let username = shortId();
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+    const newUser = new User({ email, name, password, username, profile });
+    newUser.save((err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+
+      res.status(201).json({
+        user,
+        message: "Signup success.",
+      });
+    });
   });
 };
 
 module.exports = {
-    getSignup,
+  signup,
 };
