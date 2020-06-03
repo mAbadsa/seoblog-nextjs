47550d73670dd9e19f4efe0d83a6cf71b9753338
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-03 14:10:31 +0300

    Add encryptPassword Methods in User Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
   {
@@ -51,5 +52,36 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    const _password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function () {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
