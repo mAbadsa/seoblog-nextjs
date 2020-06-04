a75f1643a1e01abbe167bf16013a5df2f36ff035
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-04 18:49:22 +0300

    Add Layout component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,11 @@
-const Index = () => <h2>مرحبا بك: الصفحة الرئيسية</h2>
+import Layout from "../components/Layout";
+const Index = () => {
+  return (
+    <Layout>
+      <h1>الصفحة الرئيسية</h1>
+      <h2>مرحبا بك</h2>
+    </Layout>
+  );
+};
 
 export default Index;
