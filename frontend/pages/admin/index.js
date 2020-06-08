198b14ad7a5f7b6e725d561797ee86511aeccc2b
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-08 03:58:27 +0300

    Create User And Admin Dashboard Component and Router

---
+++ frontend/pages/admin/index.js
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+
+const AdminIndex = () => {
+  return (
+    <Layout>
+      <h1>Admin Dashboard</h1>
+      <h2>مرحبا بك</h2>
+    </Layout>
+  );
+};
+
+export default AdminIndex;
---
+++ frontend/pages/user/index.js
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+
+const UserIndex = () => {
+  return (
+    <Layout>
+      <h1>User Dashboard</h1>
+      <h2>مرحبا بك</h2>
+    </Layout>
+  );
+};
+
+export default UserIndex;
