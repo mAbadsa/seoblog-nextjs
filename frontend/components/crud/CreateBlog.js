1a09052ad1b8cedbe8ec96daad0fc42fc0c15c60
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-15 10:57:57 +0300

    Quill modules, event handlers and localstorage

--- frontend/components/crud/CreateBlog.js
+++ frontend/components/crud/CreateBlog.js
@@ -8,19 +8,21 @@ import { tags } from "../../actions/tag";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: true });
 import "../../node_modules/react-quill/dist/quill.snow.css";
+import "./createBlog.css";
 
 const CreateBlog = ({ router }) => {
-  const [blog, setBlog] = useState({
-    title: "",
-    body: "",
-    slug: "",
-    mtitle: "",
-    mdesc: "",
-    excerpt: "",
-    photo: "",
-    categories: [],
-    tags: [],
-  });
+  const getLocalStorageItem = () => {
+    if (typeof window === "undefined") {
+      return false;
+    }
+    if (window.localStorage.getItem("blog")) {
+      return JSON.parse(window.localStorage.getItem("blog"));
+    } else {
+      return false;
+    }
+  };
+
+  const [body, setBody] = useState(getLocalStorageItem());
 
   const [values, setValues] = useState({
     error: "",
@@ -34,33 +36,33 @@ const CreateBlog = ({ router }) => {
   const {
     error,
     errorSize,
-    formData,
     success,
-    // title,
+    formData,
+    title,
     hidePublishButton,
   } = values;
 
-  const {
-    title,
-    body,
-    slug,
-    mtitle,
-    mdesc,
-    excerpt,
-    photo,
-    categories,
-    tags,
-  } = blog;
+  useEffect(() => {
+    setValues({ ...values, formData: new FormData() });
+  }, [router]);
 
   const publishBlog = () => {
     e.preventDefault();
   };
 
-  const handleChange = (e) => {
-    name = e.target.value;
+  const handleChange = (name) => (e) => {
+    const value = name === "photo" ? e.taget.files[0] : e.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value, formData });
   };
 
-  const handleBody = (e) => {};
+  const handleBody = (e) => {
+    setBody(e);
+    formData.set("body", e);
+    if (window !== "undefined") {
+      localStorage.setItem("blog", JSON.stringify(e));
+    }
+  };
 
   const CreateBlogForm = () => {
     return (
@@ -71,12 +73,14 @@ const CreateBlog = ({ router }) => {
             type="text"
             className="form-control"
             value={title}
-            onChange={handleChange}
+            onChange={handleChange("title")}
           />
         </div>
         <div className="form-group">
           <label className="text-muted">Body:</label>
           <ReactQuill
+            modules={CreateBlog.modules}
+            formats={CreateBlog.formats}
             placeholder="Write blog content..."
             value={body}
             onChange={handleBody}
@@ -91,14 +95,49 @@ const CreateBlog = ({ router }) => {
 
   return (
     <React.Fragment>
-      <div className="row py-5">
+      <div className="row py-5 bg-form-box ">
         <div className="col-md-8 offset-2 pb-5">
           <h1 className="text-center">Create New Blog</h1>
-          <CreateBlogForm />
+          {CreateBlogForm()}
+          {JSON.stringify(title)}
         </div>
       </div>
     </React.Fragment>
   );
 };
+
+CreateBlog.modules = {
+  toolbar: [
+    [{ header: 1 }, { header: 2 }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+CreateBlog.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "code-block",
+];
 
 export default withRouter(CreateBlog);
--- frontend/pages/admin/crud/blog.js
+++ frontend/pages/admin/crud/blog.js
@@ -2,12 +2,13 @@ import Layout from "../../../components/Layout";
 import Admin from "../../../components/auth/Admin";
 import CreateBlog from "../../../components/crud/CreateBlog";
 import Link from "next/link";
+import './bg.css'
 
 const Blog = () => {
   return (
     <Layout>
       <Admin>
-        <div className="row py-5 px-5">
+        <div className="row py-5 px-5 bg">
           <div className="col-md-12">
             <CreateBlog />
           </div>
