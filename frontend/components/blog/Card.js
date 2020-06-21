cae68f6573fa15354894047576fb07e4ab37dd17
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-21 14:57:42 +0300

    Show blog categories and tags

---
+++ frontend/components/blog/Card.js
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import moment from "moment";
+import renderHtml from "react-render-html";
+import Chip from "@material-ui/core/Chip";
+
+const Card = ({ blog }) => {
+  const showCategories = () => {
+    return blog.categories.map((c, i) => {
+      return (
+        <Link key={i} href={`/categories/${c.slug}`}>
+          <a className="mr-1">
+            <Chip
+              className=""
+              label={c.name}
+              color="primary"
+              variant="outlined"
+            />
+          </a>
+        </Link>
+      );
+    });
+  };
+
+  const showTags = () => {
+    return blog.tags.map((t, i) => {
+      return (
+        <Link key={i} href={`/tags/${t.slug}`}>
+          <a className="mr-1">
+            <Chip
+              className=""
+              label={t.name}
+              color="primary"
+              variant="outlined"
+            />
+          </a>
+        </Link>
+      );
+    });
+  };
+
+  return (
+    <div className="lead pb-4 pl-2 border rounded mb-2">
+      <headers>
+        <Link href={`/blogs/${blog.slug}`}>
+          <a>
+            <h2 className="py-3 font-weight-bold">{blog.title}</h2>
+          </a>
+        </Link>
+      </headers>
+      <section className="">
+        <p className="mark ml-1 py-2">
+          Written by: <span className="text-success">{blog.postedBy.name}</span>{" "}
+          | Published {moment(blog.createdAt).fromNow()}
+        </p>
+      </section>
+      <div className="row my-0 mx-0">
+        <div className="col-md-4 my-1 bg-light py-2 border rounded">
+          {showCategories()}
+        </div>
+        <div className="col-md-4 my-1 bg-light ml-1 py-2 border rounded">
+          {showTags()}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-3">__IMAGE__</div>
+        <div className="col-md-9">
+          {" "}
+          {renderHtml(blog.excerpt)}
+          <Link href={`/blogs/${blog.slug}`}>
+            <a className="btn btn-primary">Read more...</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -4,44 +4,55 @@ import { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import { listAllBlogsCategoriesTags } from "../../actions/blog";
 import { API } from "../../config";
+import moment from "moment";
+import renderHtml from "react-render-html";
+import Card from "../../components/blog/Card";
 
 const Blogs = ({ blogs, tags, categoreis, size }) => {
   const getBlogElm = () => {
     return blogs.map((blog, i) => {
       return (
         <article key={i}>
-          <div className="lead pb-4 pl-2 border rounded mb-2">
-            <headers>
-              <Link href={`/blogs/${blog.slug}`}>
-                <a>
-                  <h2 className="py-3 font-weight-bold">{blog.title}</h2>
-                </a>
-              </Link>
-            </headers>
-            <section className="">
-              <p className="mark ml-1 py-2">
-                Written by:{" "}
-                <span className="text-success">{blog.postedBy.name}</span> |
-                Published {new Date(blog.createdAt).toLocaleDateString()}
-              </p>
-            </section>
-            <section className="">
-              <p className="ml-1 py-2">Categories and Tags</p>
-            </section>
-            <div className="row">
-              <div className="col-md-4">__IMAGE__</div>
-              <div className="col-md-8">
-                {" "}
-                {blog.excerpt}
-                <Link href={`/blogs/${blog.slug}`}>
-                  <a className="btn btn-primary">Read more...</a>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <Card blog={blog} />
         </article>
       );
     });
+
+    // return blogs.map((blog, i) => {
+    //   return (
+    //     <article key={i}>
+    //       <div className="lead pb-4 pl-2 border rounded mb-2">
+    //         <headers>
+    //           <Link href={`/blogs/${blog.slug}`}>
+    //             <a>
+    //               <h2 className="py-3 font-weight-bold">{blog.title}</h2>
+    //             </a>
+    //           </Link>
+    //         </headers>
+    //         <section className="">
+    //           <p className="mark ml-1 py-2">
+    //             Written by:{" "}
+    //             <span className="text-success">{blog.postedBy.name}</span> |
+    //             Published {moment(blog.createdAt).fromNow()}
+    //           </p>
+    //         </section>
+    //         <section className="">
+    //           <p className="ml-1 py-2">Categories and Tags</p>
+    //         </section>
+    //         <div className="row">
+    //           <div className="col-md-4">__IMAGE__</div>
+    //           <div className="col-md-8">
+    //             {" "}
+    //             {renderHtml(blog.excerpt)}
+    //             <Link href={`/blogs/${blog.slug}`}>
+    //               <a className="btn btn-primary">Read more...</a>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </article>
+    //   );
+    // });
   };
 
   return (
