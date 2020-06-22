961d84715d4cba5d5300ff917b14d85cecaedb42
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-22 21:32:33 +0300

    Add SingleBlog Page

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -7,20 +7,32 @@ import { getSingleBlog } from "../../actions/blog";
 import { APP_NAME, DOMAIN_NAME, FB_APP_ID } from "../../config";
 import Chip from "@material-ui/core/Chip";
 
-const singleBlog = ({ router }) => {
+const singleBlog = ({ blog, router }) => {
   return (
     <React.Fragment>
       <Layout>
         <main>
           <article>
             <div className="container-fluid">
-              <section></section>
+              <section>{JSON.stringify(blog.blog.tags)}</section>
             </div>
           </article>
         </main>
       </Layout>
     </React.Fragment>
   );
+};
+
+singleBlog.getInitialProps = ({ query }) => {
+  return getSingleBlog(query.slug).then((data) => {
+    if (data.error) {
+    } else {
+        console.log(data);
+      return {
+        blog: data,
+      };
+    }
+  });
 };
 
 export default withRouter(singleBlog);
