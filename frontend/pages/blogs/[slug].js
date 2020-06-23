f6705034471310004ce83efa6b1ea862d203ccb2
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-23 09:35:56 +0300

    Show Single BLog with Image and Body

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -3,18 +3,91 @@ import Link from "next/link";
 import { withRouter } from "next/router";
 import { useState } from "react";
 import Layout from "../../components/Layout";
+import renderHtml from "react-render-html";
 import { getSingleBlog } from "../../actions/blog";
-import { APP_NAME, DOMAIN_NAME, FB_APP_ID } from "../../config";
+import { API, APP_NAME, DOMAIN_NAME, FB_APP_ID } from "../../config";
 import Chip from "@material-ui/core/Chip";
+import moment from "moment";
 
-const singleBlog = ({ blog, router }) => {
+const singleBlog = ({ blog }) => {
+  const showCategories = () => {
+    return blog.categories.map((c, i) => {
+      return (
+        <Link key={i} href={`/categories/${c.slug}`}>
+          <a className="mr-1">
+            <Chip
+              className=""
+              label={c.name}
+              color="primary"
+              variant="outlined"
+            />
+          </a>
+        </Link>
+      );
+    });
+  };
+
+  const showTags = () => {
+    return blog.tags.map((t, i) => {
+      return (
+        <Link key={i} href={`/tags/${t.slug}`}>
+          <a className="mr-1">
+            <Chip
+              className=""
+              label={t.name}
+              color="primary"
+              // variant="outlined"
+            />
+          </a>
+        </Link>
+      );
+    });
+  };
   return (
     <React.Fragment>
       <Layout>
         <main>
           <article>
             <div className="container-fluid">
-              <section>{JSON.stringify(blog.blog.tags)}</section>
+              <section>
+                <div className="row justify-content-center">
+                  <img
+                    className="img img-fluid featured-img"
+                    src={`${API}/blogs/photo/${blog.slug}`}
+                    alt={blog.title}
+                  />
+                </div>
+              </section>
+              <section>
+                <p className="lead py-1">
+                  Written by:{" "}
+                  <span className="text-primary">{blog.postedBy.name}</span> |
+                  Published{" "}
+                  <span className="text-primary">
+                    {moment(blog.createdAt).fromNow()}
+                    primary
+                  </span>
+                </p>
+                <div className="py-2 px-5">
+                  {showCategories()}
+                  {showTags()}
+                </div>
+              </section>
+              <div className="container">
+                <section className="blog-body">
+                  <div className="col-md-12 lead pt-5">
+                    {renderHtml(blog.body)}
+                  </div>
+                </section>
+              </div>
+              <div className="container">
+                <h4 className="text-center py-5">Related blogs</h4>
+                <hr />
+                <p>Show related blogs</p>
+              </div>
+              <div className="container">
+                <h4 className="text-center py-5">Show Comments</h4>
+              </div>
             </div>
           </article>
         </main>
@@ -27,12 +100,11 @@ singleBlog.getInitialProps = ({ query }) => {
   return getSingleBlog(query.slug).then((data) => {
     if (data.error) {
     } else {
-        console.log(data);
       return {
-        blog: data,
+        blog: data.blog,
       };
     }
   });
 };
 
-export default withRouter(singleBlog);
+export default singleBlog;
