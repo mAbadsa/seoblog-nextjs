b115ced626ebc7eb9284c10958eda4e24d4d75f2
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-06-28 14:01:13 +0300

    Add quill helper

---
+++ frontend/helpers/quill.js
@@ -0,0 +1,35 @@
+import CreateBlog from "../components/crud/CreateBlog";
+
+export const QuillModules = {
+  toolbar: [
+    [{ header: 1 }, { header: 2 }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+export const QuillFormates = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "code-block",
+];
