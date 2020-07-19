88311695c9f58e4338433723b3d1c1df4fc7719d
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-07-19 13:55:05 +0300

    integrate with google analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
 
 class MyDocument extends Document {
+  setGoogleTags() {
+    if (publicRuntimeConfig.PRODUCTION) {
+      return {
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-172961781-1');
+        `,
+      };
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -19,6 +34,11 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
           /> */}
           <link rel="stylesheet" href="/static/css/styles.css" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-172961781-1"
+          ></script>
+          <script dangerouslySetInnerHTML={this.setGoogleTags()}></script>
         </Head>
         <body>
           <Main />
